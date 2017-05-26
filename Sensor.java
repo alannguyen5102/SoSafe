9ac4dff3b5a82b99fafefcd3eb97b2b40776dbfa
Alan Nguyen <aknguyen@scu.edu>
2017-05-25 17:46:15 -0700

    Sensor is now Observable

--- Sensor.java
+++ Sensor.java
@@ -2,11 +2,24 @@
  * 
  */
 package sosafesystems;
+import java.time.LocalTime;
+import java.util.*;
 
 /**
  * @author alannguyen
  *
  */
-public abstract class Sensor {
+public abstract class Sensor extends java.util.Observable
+{
+	protected static Integer count = 0;
+	private Integer idNum;
+	private String location;
+	private Boolean powerStatus;
+	private Boolean manualStatus;
+	
+	//Need to change this
+	private LocalTime fromTime;
+	private LocalTime ToTime;
+	
 
 }
