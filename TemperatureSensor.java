3de9602ea0cda0164d4b00c86ad6bba5d080d219
Alan Nguyen <aknguyen@scu.edu>
2017-06-06 23:36:04 -0700

    added message

--- TemperatureSensor.java
+++ TemperatureSensor.java
@@ -14,6 +14,7 @@ public class TemperatureSensor extends Sensor {
 	 */
 	public TemperatureSensor(String location) {
 		super(location);
+		message = new String("FIRE");
 	}
 
 	/**
@@ -23,9 +24,11 @@ public class TemperatureSensor extends Sensor {
 	 */
 	public TemperatureSensor(String location, String fromTime, String toTime) {
 		super(location, fromTime, toTime);
+		message = new String("FIRE");
 	}
 	public TemperatureSensor(Integer idNum, String location, Boolean powerStatus, Boolean manualStatus, Boolean alarmStatus, String fromTime, String toTime) {
 		super(idNum, location, powerStatus, manualStatus, alarmStatus, fromTime, toTime);
+		message = new String("FIRE");
 	}
 
 }
